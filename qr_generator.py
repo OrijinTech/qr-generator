c7f7d8799dff87c55732f969f845dc60c52ea344
YongxiangJ <32666867+OrijinTech@users.noreply.github.com>
2023-10-08 16:08:25 +0000

    Added new functionalities.

--- qr_generator.py
+++ qr_generator.py
@@ -9,25 +9,40 @@ import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
 app_title = 'QR Code Generator'
 qr_name = 'qrcode.png'
+placeholder = 'https://'
+noQR = True
 
-def create_qr_code(url):
-    qr = qrcode.QRCode(version=5, box_size=10, border=5)
-    qr.add_data(url)
-    qr.make(fit = True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(qr_name)
-    qr_image = Image.open('qrcode.png')
-    get_img(qr_image)
+def create_qr_code(url, logo=None):
+    if url != '':
+        noQR = False
+        basewidth = 100
+        qr = qrcode.QRCode(version=5, box_size=10, border=5)
+        qr.add_data(url)
+        img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+        # if user upload logo, then add it to the qr code
+        if logo != None:
+            logo = Image.open(logo)
+            wpercent = (basewidth/float(logo.size[0]))
+            hsize = int((float(logo.size[1])*float(wpercent)))
+            logo = logo.resize((basewidth, hsize), Image.LANCZOS)
+            pos = ((img.size[0] - logo.size[0]) // 2,(img.size[1] - logo.size[1]) // 2)
+            img.paste(logo, pos)
+            qr.make(fit = True)
+        # save img 
+        img.save(qr_name)
+        qr_image = Image.open('qrcode.png')
+        get_img(qr_image)
+    else:
+        noQR = True
     return qr_name
 
 
 def create_app_instance(title):
-    placeholder = 'https://'
     st.title(title)
     url = st.text_input(label='Enter URL', placeholder=placeholder)
-    img_name = create_qr_code(url)
+    userimg = st.file_uploader(label='Upload Picture for QR Code Decoration', key='upload')
+    img_name = create_qr_code(url, userimg)
     img = open(img_name,'rb')
-    st.file_uploader(label='Upload Picture', key='upload')
     st.download_button(label='Download', data=img, file_name='img.png', mime="image/png")
 
 
